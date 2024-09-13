890db97a7b7f9f588f762418577f12e6510a3f0f
Abrar892 <m.abrar.j40@gmail.com>
2024-09-13 12:03:25 -0700

    TASK

---
+++ Week 2/20.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+main()
+{
+ system("color 5F ");
+cout << "   .....                eeeee   eeeee          ............       " <<endl;
+cout << "   ......              e     e e     e         ..........         " <<endl;
+cout << "   .......                eee  e     e         ........           " <<endl;
+cout << "   ........             ee     e     e         ........           " <<endl;
+cout << "     .......           eeeeeee  eeeee          .......            " <<endl;
+cout << "       ......        .....................     .......            " <<endl;
+cout << "        ......          C E N T U R Y          ......             " <<endl;
+cout << "         ......      .....................     .....              " <<endl;
+cout << "            **         eeeee eeee e   e  eeeee    **              " <<endl;
+cout << "         ___||___        e   e     e e     e   ___||___           " <<endl;
+cout << "        |        |       e   eee    e      e  |        |          " <<endl;
+cout << "________|________|_____  e   e     e  e    e__|________|__        " <<endl;
+cout << "                         e   eeee e    e   e                      " <<endl;
+
+
+
+
+
+
+
+
+
+
+
+
+}
