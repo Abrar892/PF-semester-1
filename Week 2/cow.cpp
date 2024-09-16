fd79fe44c23e7b362c5dbff0bae073cc0b3128b0
Abrar892 <m.abrar.j40@gmail.com>
2024-09-15 19:21:31 -0700

    TASK

---
+++ Week 2/cow.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+main()
+{
+cout << "   ------------------------                                   "<<endl;
+cout << "                                    "<<endl;
+cout << "          o    ^__^                          "<<endl;
+cout << "               (oo)\\___________                    "<<endl;
+cout << "            o  (__)\\           )\\/\\                     "<<endl;
+cout << "                   ||--------w |              "<<endl;
+cout << "                   ||         ||              "<<endl;
+
+}
